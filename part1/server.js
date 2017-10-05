8d3e8b1d5b0d90bbf3e24e7c399421ef2560f148
bmcgrane302 <bmcgrane302@yahoo.com>
2017-10-04 17:15:52 -0700

    express

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var path = require('path');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/hello', function(req, res) {
+  res.send("Hello");
 });
+
+app.post('/create/:name', function(req, res) {
+  let obj = {
+    "id":1,
+    "name":req.params.name
+  }
+
+  res.json(obj);
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+                     //two underscores
+})
+
+app.get('/verify/:age', function(req, res){
+  let age = parseInt(req.params.age);
+  if(age>=13){
+    res.sendStatus(200);
+  }else{
+    res.sendStatus(403);
+  }
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,64 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
+var path = require('path');
+
+
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+
+app.post('/create/:name/:age', function(req, res) {
+  let obj = {
+    "name": req.params.name,
+    "age": req.params.age
+  }
+
+  fs.readFile('./storage.json', 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    data = JSON.parse(data);
+    console.log("prestige worldwide");
+    data.push(obj);
+
+    fs.writeFile('./storage.json', JSON.stringify(data), function(err) {
+      if (err) {
+        throw err;
+      }
+
+    })
+  })
+  res.send("We are storing your name and age");
+
+});
+
+app.get('/', function(req, res){
+  fs.readFile('./storage.json', 'utf8', function(err, data){
+    if(err){
+      throw err;
+    }
+    let currentData = JSON.parse(data);
+    res.json(currentData);
+  })
+})
+
+app.get('/:name', function(req, res) {
+
+  fs.readFile('./storage.json', 'utf8', function(err, data) {
+    let user = req.params.name;
+    data = JSON.parse(data);
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].name.toLowerCase() === user.toLowerCase()) {
+        res.json(data[i]);
+        return;
+      }
+    }
+    res.sendStatus(400);
+  })
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
